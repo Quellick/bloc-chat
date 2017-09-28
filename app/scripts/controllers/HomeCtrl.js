ebd9f3f35f7b612eb34001bc45805210f27c9b95
Quellick <Quellick@gmail.com>
2017-09-28 14:48:08 -0700

    checkpoint 2

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,9 @@
 (function() {
-    function HomeCtrl() {
+    function HomeCtrl(Room) {
+      this.rooms = Room.all; //sets the current room values from the values of the firebase room object.
     }
 
-    angular
-        .module('blocChat')
-        .controller('HomeCtrl', [HomeCtrl]);
+    angular //
+        .module('blocChat') //registers which app the controller is for.
+        .controller('HomeCtrl', ["Room", HomeCtrl]); //registers controller with dependancy injector ('function name', ["dependancy name", Actualy Function Name])
 })();
