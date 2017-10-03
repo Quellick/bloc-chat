ee98e1b36e793e3b9b2ce0ff20a674049e8b00c7
Quellick <Quellick@gmail.com>
2017-10-02 21:02:01 -0700

    commit

--- app/scripts/controllers/ModalInstanceCtrl.js
+++ app/scripts/controllers/ModalInstanceCtrl.js
@@ -1,7 +1,11 @@
 (function() {
-    function ModalInstanceCtrl($uibModalInstance) {
+    function ModalInstanceCtrl($uibModalInstance, Room) {
         this.cancel = function() {
             $uibModalInstance.dismiss('dismiss')
+        }
+        this.submit = function () {
+          Room.$add(this.newRoom);
+          $uibModalInstance.close();
         }
     }
 
