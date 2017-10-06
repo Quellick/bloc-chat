6c5d2782898273b2d5da73275174620763cd17d6
Quellick <Quellick@gmail.com>
2017-10-06 10:05:22 -0700

    checkpoint 3 finished

--- app/scripts/controllers/ModalInstanceCtrl.js
+++ app/scripts/controllers/ModalInstanceCtrl.js
@@ -1,15 +1,14 @@
 (function() {
-    function ModalInstanceCtrl($uibModalInstance) {
+    function ModalInstanceCtrl($uibModalInstance, Room) {
         this.cancel = function() { //removes the modal if user clicks cancel.
           $uibModalInstance.dismiss('canceled')
         }
         this.submit = function () { //remove the modal if user submits new room name.
-          this.newRoom
-          $uibModalInstance.close();
+          $uibModalInstance.close(this.newRoom);
         }
     }
 
     angular
         .module('blocChat')
-        .controller('ModalInstanceCtrl', ['$uibModalInstance', ModalInstanceCtrl])
+        .controller('ModalInstanceCtrl', ['$uibModalInstance', "Room", ModalInstanceCtrl])
 })()
