14ad938caef6adcec2f7c664473481d3af336c49
Quellick <Quellick@gmail.com>
2017-10-17 11:04:25 -0700

    commit

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -4,11 +4,16 @@
     var ref = firebase.database().ref().child("Messages");
     var messages = $firebaseArray(ref);
 
-    Message.all = messages; //populates the Message object with all the available messages
+    //populates the Message object with all the available messages
+    Message.all = messages; 
 
     Message.getByRoomId = function(roomId) {
        //console.log(messages);
        return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+    };
+
+    Message.send = function(newMessage) {
+        // Send method logic
     };
 
     return Message;
